ea12b4b1982eb75f8b35bb76717521dd82114c2f
José Luis Lafuente <jl@lafuente.dev>
2025-08-26 12:09:42 +0200

    Add a nix shell
    While running the examples I noticed that some binaries weren't present
    on my system (uv and fga). A Nix dev shell should help with that.

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells = {
+          default =
+            with pkgs;
+            mkShell {
+              buildInputs = [
+                python3
+                uv
+                openfga
+                openfga-cli
+              ];
+            };
+        };
+      }
+    );
+}
